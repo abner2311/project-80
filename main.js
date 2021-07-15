f804af23855ee47f8e70a4bfe0caf2a2659d4116
abner2311 <85498429+abner2311@users.noreply.github.com>
2021-07-15 15:29:40 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var GuestName= document.getElementById("name1").value;
+name_of_guest.push(GuestName);
+var GuestName= document.getElementById("name2").value;
+name_of_guest.push(GuestName);
+var GuestName= document.getElementById("name3").value;
+name_of_guest.push(GuestName);
+var GuestName= document.getElementById("name4").value;
+name_of_guest.push(GuestName);
+
+
+function sorting(){
+    name_of_guest.sorting;
+}
